e4e4a31e34dc040335c4d0e32701ae3382f7c717
Dmitrii Borovoi <dmborovoy@gmail.com>
2017-10-23 15:48:01 +1100

    Initial commit

---
+++ gateway/src/main/java/com/dxboro/lango/gateway/LangoApplication.java
@@ -0,0 +1,12 @@
+package com.dxboro.lango.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangoApplication.class, args);
+	}
+}
